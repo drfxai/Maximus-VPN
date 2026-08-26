86ef2658a17472dcb268cf525b64da9757dc7804
drfxai <drfxai@gmail.com>
2026-08-26 11:52:34 +0330

    Fix: Close string literal on line 10 - Add missing closing quote

--- app/src/test/kotlin/com/drfxai/maximusvpn/DebugVmessTest.kt
+++ app/src/test/kotlin/com/drfxai/maximusvpn/DebugVmessTest.kt
@@ -7,7 +7,7 @@ class DebugVmessTest {
 
     @Test
     fun debug_vmess() {
-        val uri = "vmess://eyJ2IjoiMiIsInBzIjoiVk1lc3MgTm9kZSIsImFkZCI6InVzLmV4YW1wbGUuY29tIiwicG9ydCI6IjQ0MyIsImlkIjoiZDQ2YjRlYjItNmFiYy00YjM3LTg2Y2QtZDk3MmUxYzQ1Njc4IiwiYWlkIjoiMCIsInNjeSI6ImF1d[...]
+        val uri = "vmess://eyJ2IjoiMiIsInBzIjoiVk1lc3MgTm9kZSIsImFkZCI6InVzLmV4YW1wbGUuY29tIiwicG9ydCI6IjQ0MyIsImlkIjoiZDQ2YjRlYjItNmFiYy00YjM3LTg2Y2QtZDk3MmUxYzQ1Njc4IiwiYWlkIjoiMCIsInNjeSI6ImF1d"
         val result = VlessParser.parse(uri)
         println("Result: $result")
         if (result is com.drfxai.maximusvpn.core.AppResult.Error) {
