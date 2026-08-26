0d44daa618c0e82cde36e6d5264ef07e63b14d90
drfxai <drfxai@gmail.com>
2026-08-26 18:05:53 +0330

    Set Android default release version to 2.5.2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
     applicationId = "com.drfxai.maximusvpn"
     minSdk = 24
     targetSdk = 36
-    versionCode = (providers.gradleProperty("versionCode").orNull?.toIntOrNull() ?: 2)
-    versionName = providers.gradleProperty("versionName").orElse("2.0.0").get()
+    versionCode = (providers.gradleProperty("versionCode").orNull?.toIntOrNull() ?: 252)
+    versionName = providers.gradleProperty("versionName").orElse("2.5.2").get()
 
     testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
